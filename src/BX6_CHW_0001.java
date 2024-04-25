2b90bf397657cef73f75454a1235057ce99b8419
SnigdhS <144094527+SnigdhS@users.noreply.github.com>
2024-04-25 17:19:32 +0530

    first commit

---
+++ src/BX6_CHW_0001.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class BX6_CHW_0001 {
+    public static void main(String[] args) {
+      // Prime number or no
+      /*
+
+          num >1 ;
+          if(n%2 == 0 || n%3 == 0) {
+          sout ("not prime number")
+          }else if (n = 1 || n = 2 || n%n == 0)
+          {
+          sout("prime number")
+
+          if (num%num ==0){
+
+          }
+          }
+
+       */
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = s.nextInt();
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+    }
+    public static boolean isPrime(int n) {
+            if (n <= 1) {
+                return false;
+            }
+            for (int i = 2; i < Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+}
+
+
