c6559eaaaddd1f4c9a68164cab285ba218d398d9
SnigdhS <144094527+SnigdhS@users.noreply.github.com>
2024-05-09 16:38:48 +0530

    update codefor (int j= 0; j<arr[i].length; j++ ){

--- src/Home_work_04_May/element_exists_in_a_2D_array.java
+++ src/Home_work_04_May/element_exists_in_a_2D_array.java
@@ -2,22 +2,25 @@ package Home_work_04_May;
 
 public class element_exists_in_a_2D_array {
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+        int[][] arr = {{1, 2, 3, 5},{4, 5, 6,5},{7, 8, 9,2}};
         int count = 0;
         //boolean elements  == 8;
         for(int i = 0; i<arr.length; i++){
          //  System.out.print("i -->" +i);
-            for (int j= 0; j<=i; j++ ){
+            for (int j= 0; j<arr[i].length; j++ ){
               //  System.out.print(j);
                 count =count + i;
-
             }
-        }
-        if (count == 8) {
-            System.out.println("element =8"+ true);
+
         }
      // count = + count ;
-       // System.out.println(count);
+     //   System.out.println(count);
+        if (count == 8) {
+            System.out.println("element ="+ count + true );
+        } else
+        {
+            System.out.println("element ="+ count +false);
+        };
     }
 }
 /*
